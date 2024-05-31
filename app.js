91cdb2a1937e52a5fceb04b59b1177554ce4b9fa
Chibuzor Collins Dabelechi <165359829+ChibuzorCollinsDab@users.noreply.github.com>
2024-05-31 06:13:52 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const a = document.getElementById("icon-cart");
+   b = document.getElementById("cart");
+a.addEventListener("click", c=>{
+    b.style.display === "none" ? b.style.display = "block" : b.style.display = "none"
+}
+);
